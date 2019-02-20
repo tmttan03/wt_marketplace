f249cd369d78979ef6180ed73057bd20dd24c362
tmttan03 <tmttan03@gmail.com>
2019-02-20 13:40:47 +0800

    Created a Product Album Model

--- marketplace/products/models.py
+++ marketplace/products/models.py
@@ -42,3 +42,12 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
+
+
+class ProductAlbum(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to='product_images/',default='no-thumb.png')
+	uploaded_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.product.name + " Image"
