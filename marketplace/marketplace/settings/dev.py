6a4818c052cf5059206ebf8c342f6f5e99d21705
tmttan03 <tmttan03@gmail.com>
2019-02-26 15:03:08 +0800

    Create file to store the credentials for smtp

--- marketplace/marketplace/settings/dev.py
+++ marketplace/marketplace/settings/dev.py
@@ -1,4 +1,5 @@
 from .base import *
+from .credentials import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -9,12 +10,13 @@ SECRET_KEY = 'kgd_mg)fl885(97$8+k*bc^84hxu#*^3ewj_84f&qepvrf+!x1'
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tmttan03@gmail.com'
-EMAIL_HOST_PASSWORD = 'hdilt2201400117'
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = EMAIL_USE_TLS
+EMAIL_HOST = EMAIL_HOST
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_PORT = EMAIL_PORT
 
 try:
     from .local import *
